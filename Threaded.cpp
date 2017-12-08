8bbfd991d1189407961a3c29cae8be2f34ed0020
Jonathan D. Lettvin <jlettvin@translations.com>
2017-12-08 11:50:52 -0500

    Fixed Queue to limit maximum size

--- Threaded.cpp
+++ Threaded.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 //______________________________________________________________________________
 // https://juanchopanzacpp.wordpress.com/2013/02/26/concurrent-queue-c11/
-// modified from original by trivial renaming of members and stacking pop.
+// modified from original by trivial renaming of members
 template <typename T>
 class Queue
 {
@@ -27,41 +27,71 @@ class Queue
 public:
 //------
 
+	Queue (size_t a_maximum=32) : m_maximum (a_maximum) {}
+
 	//--------------------------------------------------------------------------
 	T pop()
 	{
 		std::unique_lock<std::mutex> mlock(m_mutex);
 		while (m_queue.empty())
 		{
-			m_condition.wait(mlock);
+			m_available.wait(mlock);
 		}
 		auto item = m_queue.front();
 		m_queue.pop();
+
+		// Needs confirmation
+		m_full.notify_one();
+
 		return item;
 	} // pop ()
 
 	//--------------------------------------------------------------------------
 	void pop(T& a_item)
 	{
-		a_item = pop ();
+		std::unique_lock<std::mutex> mlock(m_mutex);
+		while (m_queue.empty())
+		{
+			m_available.wait(mlock);
+		}
+		a_item = m_queue.front();
+		m_queue.pop();
+
+		// Needs confirmation
+		m_full.notify_one();
+
 	} // pop (item)
 
 	//--------------------------------------------------------------------------
 	void push(const T& a_item)
 	{
 		std::unique_lock<std::mutex> mlock(m_mutex);
+
+		// Needs confirmation
+		while (m_queue.size () == m_maximum)
+		{
+			m_full.wait(mlock);
+		}
+
 		m_queue.push(a_item);
 		mlock.unlock();
-		m_condition.notify_one();
+		m_available.notify_one();
 	} // push (item) const
 
 	//--------------------------------------------------------------------------
 	void push(T&& a_item)
 	{
 		std::unique_lock<std::mutex> mlock(m_mutex);
+
+		// Needs confirmation
+		while (m_queue.size () == m_maximum)
+		{
+			m_full.wait(mlock);
+		}
+
 		m_queue.push(std::move(a_item));
 		mlock.unlock();
-		m_condition.notify_one();
+		m_available.notify_one();
 	} // push (item) move
 
 //------
@@ -70,7 +100,9 @@ private:
 
 	std::queue<T> m_queue;
 	std::mutex m_mutex;
-	std::condition_variable m_condition;
+	std::condition_variable m_available;
+	std::condition_variable m_full;       // TODO prevent more than max
+	size_t m_maximum{32};                 // TODO prevent more than max
 
 }; // class Queue
 
@@ -83,12 +115,11 @@ public:
 //------
 
 	//--------------------------------------------------------------------------
-	Distribute () {}
+	Distribute (size_t a_max=32) : m_cpus (a_max), m_queue (a_max) {}
 
 	//--------------------------------------------------------------------------
 	void operator()(vector<string>& a_tokens)
 	{
-		m_cpus = thread::hardware_concurrency() - 1;
 		// One less workers than cpus
 		for (size_t thid=1; thid < m_cpus; ++thid)
 		{
@@ -141,12 +172,13 @@ private:
 #ifdef MAIN
 //______________________________________________________________________________
 int main() {
+	auto cpus = std::thread::hardware_concurrency();
 	std::vector<std::string> tokens{{
 		"a","b","c","d","e","f","g","h","i","j","k","l","m",
 		"n","o","p","q","r","s","t","u","v","w","x","y","z"
 	}};
 
-	Lettvin::Distribute distribute;
+	Lettvin::Distribute distribute (cpus);
 
 	distribute (tokens);
 
